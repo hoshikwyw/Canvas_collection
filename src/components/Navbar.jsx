04bdc46801b46338e0a461d08ed8671dd9c306d4
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-03-08 12:52:26 +0630

    Frame set up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import logo from '/favicon.ico'
 
 const Navbar = () => {
   return (
-    <div>
-      navbar
-    </div>
+    <nav className=" w-full h-fit">
+      <div className=' w-full h-fit bg-transparent border-b border-blue-200 px-5 py-2 flex justify-between items-center'>
+        <div className="">
+          <a href="#">
+            <h1 className=' font-semibold text-xl tracking-wide font-mono'>Canvas Collection</h1>
+          </a>
+        </div>
+        <div className=" flex items-center gap-5">
+          <h2 className=' text-lg font-semibold font-mono'>Hello User !!</h2>
+          <img className=' w-8 h-8 rounded-[50%]' src={logo} alt="logo" />
+        </div>
+      </div>
+    </nav>
   )
 }
 
